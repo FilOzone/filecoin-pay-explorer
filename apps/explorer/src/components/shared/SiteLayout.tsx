1fe5a43a3f721f605c6187bb48c98f2ca29f6c5f
Puspendra Mahariya <95584952+silent-cipher@users.noreply.github.com>
2026-01-11 21:35:58 +0530

    feat(explorer): add plausible analytics integration (#73)

--- apps/explorer/src/components/shared/SiteLayout.tsx
+++ apps/explorer/src/components/shared/SiteLayout.tsx
@@ -1,5 +1,6 @@
 import { clsx } from "clsx";
 import localFont from "next/font/local";
+import Script from "next/script";
 import type { ReactNode } from "react";
 
 import { Footer, Navigation, Providers } from "@/components/shared";
@@ -25,6 +26,14 @@ type SiteLayoutProps = Readonly<{ children: ReactNode }>;
 function SiteLayout({ children }: SiteLayoutProps) {
   return (
     <html lang='en'>
+      <Script async defer src='https://plausible.io/js/pa-69jF95tDk97_PjEsmdMd6.js' />
+      <Script
+        id='next-plausible-init'
+        // biome-ignore lint/security/noDangerouslySetInnerHtml: plausible init script
+        dangerouslySetInnerHTML={{
+          __html: `window.plausible = window.plausible || function() { (plausible.q=plausible.q||[]).push(arguments) }, plausible.init = plausible.init || function(i) { plausible.o = i || {} }; plausible.init()`,
+        }}
+      />
       <body
         className={clsx(
           funnelSans.variable,
