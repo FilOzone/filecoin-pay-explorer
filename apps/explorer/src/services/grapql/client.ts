c2af583da0012e7d0016220d29968a791a681030
silent-cipher <puspendramahariya@gmail.com>
2026-01-14 18:40:26 +0530

    refactor(explorer): remove unused code

--- apps/explorer/src/services/grapql/client.ts
+++
@@ -1,9 +0,0 @@
-import { GraphQLClient } from "graphql-request";
-
-export const graphqlClient = new GraphQLClient(process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT as string);
-
-export const executeQuery = async <T>(
-  query: string,
-  // biome-ignore lint/suspicious/noExplicitAny: GraphQL variables can be of any type
-  variables?: Record<string, any>,
-): Promise<T> => graphqlClient.request<T>(query, variables);
