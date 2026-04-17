4506ab8dfa37fe00d80a0d319be3fcdf5ff3a9ba
Julian Gruber <julian@juliangruber.com>
2026-04-17 16:41:17 +0200

    fix default `DailyTokenMetric.accumulatedFees` to zero

--- packages/subgraph/src/utils/metrics/core.ts
+++ packages/subgraph/src/utils/metrics/core.ts
@@ -86,6 +86,7 @@ export class MetricsEntityManager {
       metric.activeRailsCount = ZERO_BIG_INT;
       metric.uniqueHolders = ZERO_BIG_INT;
       metric.totalLocked = ZERO_BIG_INT;
+      metric.accumulatedFees = ZERO_BIG_INT;
     }
 
     return metric;
