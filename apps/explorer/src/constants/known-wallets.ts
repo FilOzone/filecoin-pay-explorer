d28d773722acc13e5f2e02226ace8a5ddcfbada5
Julian Gruber <julian@juliangruber.com>
2026-04-29 10:49:52 +0200

    docs

--- apps/explorer/src/constants/known-wallets.ts
+++ apps/explorer/src/constants/known-wallets.ts
@@ -1,3 +1,4 @@
+// Use lowercase, not checksummed addresses
 export const knownWallets: Record<string, string> = {
   "0x3c1ae7a70a2b51458fcb7927fd77aae408a1b857": "Storacha",
   "0x3e4e5f067cfda2f16aade21912b8324c3d9624f8": "Tippy",
