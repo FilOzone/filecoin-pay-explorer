1f9347e00a7657934831b4599ce9396214ee88fd
Julian Gruber <julian@juliangruber.com>
2026-05-04 12:59:39 +0200

    add dealbot to known wallets

--- apps/explorer/src/constants/known-wallets.ts
+++ apps/explorer/src/constants/known-wallets.ts
@@ -3,4 +3,5 @@ export const knownWallets: Record<string, string> = {
   "0x3c1ae7a70a2b51458fcb7927fd77aae408a1b857": "Storacha",
   "0x3e4e5f067cfda2f16aade21912b8324c3d9624f8": "Tippy",
   "0xd19d84c77bbb901971e460830e310933a210dbaa": "PinMe",
+  "0xa5f90bc2aa73a2e0bad4d7092a932644d5dd5d71": "DealBot",
 };
