0fe63356d494535c2c54559889407b53e4ea42d9
Phi-rjan <orjan.roren@gmail.com>
2026-07-10 08:55:16 +0200

    fix: show network selector on mobile (#246)
    * fix: show network selector on mobile
    
    * fix: adjust navigation spacing
    
    * chore: remove unused mobile network selector screenshot
    
    ---------
    
    Co-authored-by: silent-cipher <puspendramahariya@gmail.com>

--- apps/explorer/src/components/shared/Navigation/Navigation.tsx
+++ apps/explorer/src/components/shared/Navigation/Navigation.tsx
@@ -5,6 +5,7 @@ import { MobileNavigation } from "@filecoin-foundation/ui-filecoin/Navigation/Mo
 import { Section, type SectionProps } from "@filecoin-foundation/ui-filecoin/Section/Section";
 import { useNavigationItems } from "@/hooks/useNavigationItems";
 import { HomeLogoIconLink } from "./components/HomeLogoIconLink";
+import NetworkOptions from "./components/NetworkOptions";
 import { DesktopNavigation } from "./DesktopNavigation";
 
 type NavigationProps = {
@@ -17,11 +18,17 @@ function Navigation({ backgroundVariant }: NavigationProps) {
   return (
     <Section as='header' backgroundVariant={backgroundVariant}>
       <Container>
-        <nav className='flex items-center justify-between py-8 xl:gap-24'>
-          <HomeLogoIconLink />
+        <nav className='flex flex-col gap-8 py-8 xl:flex-row xl:items-center xl:justify-between xl:gap-24'>
+          <div className='flex w-full items-center justify-between xl:w-auto'>
+            <HomeLogoIconLink />
 
-          <div className='block xl:hidden'>
-            <MobileNavigation items={mobileNavigationItems} HomeLogoIconLinkComponent={HomeLogoIconLink} />
+            <div className='block xl:hidden'>
+              <MobileNavigation items={mobileNavigationItems} HomeLogoIconLinkComponent={HomeLogoIconLink} />
+            </div>
+          </div>
+
+          <div className='block w-full -mb-12 xl:hidden'>
+            <NetworkOptions />
           </div>
 
           <div className='hidden xl:block xl:flex-1'>
